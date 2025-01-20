8dab59da8f9245c75e7c195674b47ccb0c4b3de7
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-01-20 09:38:03 +0700

    fix: typo stay tuned

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="w-full text-center p-4 pb-6 bg-transparent text-white flex-shrink-0">
-      <p className='font-[family-name:var(--font-montserrat)]'>Stay tune for Garuda Hacks 6.0!</p>
+      <p className='font-[family-name:var(--font-montserrat)]'>Stay tuned for Garuda Hacks 6.0!</p>
       <p className='font-[family-name:var(--font-montserrat)]'>24-26 July 2025</p>
     </footer>
   );

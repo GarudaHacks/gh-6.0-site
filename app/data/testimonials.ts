507877d2dfa2f362171feed350e7e8680ecd67f7
Winner R. Rasendriya <118193112+HykoU31@users.noreply.github.com>
2025-04-14 11:16:36 +0700

    Testimonials
    3 testimonials tab that can be expanded which will blur the screen around it focusing to the single expanded tab in the center of the page

---
+++ app/data/testimonials.ts
@@ -0,0 +1,21 @@
+export const testimonialsData = [
+    {
+      name: "Lorem Ipsum",
+      role: "Teacher",
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nunc mi. Vivamus iaculis libero vel mollis eleifend. Morbi dapibus fermentum tincidunt.",
+      image: "/image1.jpg"
+    },
+    {
+      name: "Lorem Ipsum",
+      role: "Teacher",
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nunc mi. Vivamus iaculis libero vel mollis eleifend. Morbi dapibus fermentum tincidunt.",
+      image: "/image2.jpg"
+    },
+    {
+      name: "Lorem Ipsum",
+      role: "Teacher",
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nunc mi. Vivamus iaculis libero vel mollis eleifend. Morbi dapibus fermentum tincidunt.",
+      image: "/image3.jpg"
+    }
+  ];
+  

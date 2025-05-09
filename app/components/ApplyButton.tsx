54dec62da17c6bfbb73d48d637549e410323b418
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-05-09 22:13:08 +0700

    style: application button

--- app/components/ApplyButton.tsx
+++ app/components/ApplyButton.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function ApplyButton() {
-    // change to true when applications open
-    const openApplications = false
-    const buttonLabel = openApplications ? "Apply" : "Applications closed" 
+  // change to true when applications open
+  const openApplications = false;
+  const buttonLabel = openApplications
+    ? "Apply"
+    : "Registration is not yet open";
 
-    return (
-        <button 
-        disabled={!openApplications}
-        className={
-            openApplications 
-                ? "rounded-md px-6 py-2 border-red-400 border-[1px] bg-gradient-to-tr from-red-400 to-red-600 font-bold font-sans text-white hover:opacity-80"
-                : "rounded-md px-6 py-2 border-gray-500 border-[1px] bg-gradient-to-br from-gray-500 to-gray-600 cursor-not-allowed font-bold font-sans text-white hover:opacity-80"}>
-            {buttonLabel}
-        </button>
-    )
+  return (
+    <button
+      disabled={!openApplications}
+      className={
+        openApplications
+          ? "rounded-md px-6 py-2 border-red-400 border-[1px] bg-gradient-to-tr from-red-400 to-red-600 font-bold font-sans text-white hover:opacity-80"
+          : "rounded-md px-6 py-2 border-gray-500 border-[1px] bg-gradient-to-br from-gray-500 to-gray-600 cursor-not-allowed font-bold font-sans text-white hover:opacity-80"
+      }
+    >
+      {buttonLabel}
+    </button>
+  );
 }
 
-export default ApplyButton
+export default ApplyButton;

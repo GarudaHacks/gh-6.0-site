14ad1b1128b00a753a87c875d3c3162b101e5b87
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-06-01 11:42:30 +0700

    chore: change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/site.webmanifest" />
+      </head>
       <body
         className={`w-full overflow-x-hidden overflow-y-scroll flex flex-col bg-background antialiased ${montserrat.variable}`}
       >
